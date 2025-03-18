79e4786f71d9941cc3b0e15c2bac8dcd88d8d183
Adam Haigh <adam.haigh@takepayments.com>
2025-03-18 15:25:21 +0000

    Added responsive layout.

--- WPF Tutorial/MainWindow.xaml.cs
+++ WPF Tutorial/MainWindow.xaml.cs
@@ -13,27 +13,8 @@ namespace WPF_Tutorial;
 
 public partial class MainWindow : Window
 {
-    private bool _running = false;
     public MainWindow()
     {
         InitializeComponent();
-    }
-
-    private void btnToggleRun_Click(object sender, RoutedEventArgs e)
-    {
-        if (_running)
-        {
-            // stop
-            tbStatus.Text = "Stopped";
-            btnToggleRun.Content = "Run";
-        }
-        else
-        {
-            // run
-            tbStatus.Text = "Running";
-            btnToggleRun.Content = "Stop";
-        }
-
-        _running = !_running;
     }
 }
